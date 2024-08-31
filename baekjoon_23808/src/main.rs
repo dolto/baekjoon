1fefa4892d49a7d7e2086e6c4472102f01774112
= <=>
2024-08-31 10:46:17 +0900

    day32

---
+++ baekjoon_23808/src/main.rs
@@ -0,0 +1,47 @@
+use std::io::{stdin, stdout, Write};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let input = input.trim().parse::<i32>().unwrap();
+
+    let mut output = String::new();
+    for _ in 0..input * 2 {
+        for _ in 0..input {
+            output.push_str("@");
+        }
+        for _ in 0..input {
+            output.push_str("   ");
+        }
+        for _ in 0..input {
+            output.push_str("@");
+        }
+        output.push_str("\n");
+    }
+    for _ in 0..input {
+        for _ in 0..input * 5 {
+            output.push_str("@");
+        }
+        output.push_str("\n");
+    }
+    for _ in 0..input {
+        for _ in 0..input {
+            output.push_str("@");
+        }
+        for _ in 0..input {
+            output.push_str("   ");
+        }
+        for _ in 0..input {
+            output.push_str("@");
+        }
+        output.push_str("\n");
+    }
+    for _ in 0..input {
+        for _ in 0..input * 5 {
+            output.push_str("@");
+        }
+        output.push_str("\n");
+    }
+
+    stdout().write_all(format!("{output}").as_bytes()).unwrap();
+}
